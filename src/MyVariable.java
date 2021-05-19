48d7d0a45b97f30f0edfeecdfc2b98c7262055e2
Romuald-Big-Screen <romuald.torro@gmail.com>
2021-05-19 08:46:12 +0200

    Modify variables long manner

--- src/MyVariable.java
+++ src/MyVariable.java
@@ -1,15 +1,27 @@
 public class MyVariable {public static void main(String[] args)    {
+    int savings = 500;
+    int ongoingAllowance = 2000;
 
-    //Déclarez la variable int ongoingAllowance
-    int ongoingAllowance = 200;
-    //Déclarez la variable int savings
-    int savings = 3000;
+        //Ajoutez 100 à votre épargne (Yeah!)
+        savings = savings + 100;
+
+        //Enlevez 50 à votre indemnité (Snif)
+        ongoingAllowance = ongoingAllowance - 50;
+
+        //Faites une mise à jour sur votre délai d'épargne
+        int numberOfDaysToSave = (5000 - ongoingAllowance) / 500;
+
+        //Mettez à jour à nouveau votre indemnité (encore)
+        ongoingAllowance = ongoingAllowance + (30 - 10) * 7;
+
+
+
 
     //TODO : Déclarez une variable int nommée bonusAllowance avec la valeur 500
     int bonusAllowance = 500;
 
     //Affiche le résultat
-    System.out.println("You have an ongoing allowance of " + ongoingAllowance +"\nYour savings amount is " + savings +"\nYou have a bonus allowance of " + bonusAllowance);
+    System.out.println(ongoingAllowance);
 
 }
 }
